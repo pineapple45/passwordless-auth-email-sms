b7279b4d48cc5fbe4b58dc2ecef254bf0eefcb60
pineapple45 <gomraanmol@gmail.com>
2021-08-20 14:39:32 +0530

    add mongodb and apollo-server-express

---
+++ api/src/apollo/index.ts
@@ -0,0 +1,6 @@
+import { mergeSchemas } from 'apollo-server-express'
+
+export const schemaObject = mergeSchemas({
+    schemas: [],
+    resolvers: []
+})
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,20 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express , { Express , Request, Response} from 'express'
+import mongoose from 'mongoose'
+import { ApolloServer } from 'apollo-server-express'
+import { schemaObject } from './apollo'
 
 const app: Express = express()
 
 app.use(express.json())
 
+// Initialising apollo server
+const apolloServer = new ApolloServer({
+    schema: schemaObject
+})
+
+apolloServer.applyMiddleware({app: app, cors: false})
 
 app.get('/',(req,res) => {
     res.send('<h1>Hello World</h1>')
@@ -13,6 +22,17 @@ app.get('/',(req,res) => {
 
 const PORT  = process.env.NODE_ENV === 'production' ? process.env.PORT : 4000;
 
-app.listen(PORT, () => {
-    console.log(`Listening to PORT ${PORT}...`)
+
+mongoose.connect(process.env.MONGO_URI as string , {
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
+ useFindAndModify: false,
+ useCreateIndex: true
+}).then(() => {
+    console.log('conected to mongodb...')
+    app.listen(PORT, () => {
+        console.log(`Listening to PORT ${PORT}...`)
+    })
+}).catch((err) => {
+    console.log(err)
 })
